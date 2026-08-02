3161627ae04bda68bd8160a022fbf55c27b47f84
ortacmustafa78-byte <ortacmustafa78@gmail.com>
2026-08-02 22:48:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
    ============================================ */
 const COMPANY = {
     email: "info@expressraeumung.de",
-    phone: "+49 1522 8457333"
+    phone: "+4915228457333"
 };
 
 const FORM_ENDPOINT = ""; // Add your endpoint here (Resend, Formspree, etc.)
